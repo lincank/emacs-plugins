12bebd0b0a58284c778f2bbd8d66aac1a473e1df
Menglong Tan <tanmenglong@gmail.com>
2012-04-03 18:05:19 +0800

    up

--- init/ac-settings.el
+++ init/ac-settings.el
@@ -3,6 +3,6 @@
 			 "~/.emacs.d/emacs-plugins/common-settings/auto-complete")
 (require 'auto-complete-config)
 (add-to-list 'ac-dictionary-directories
-			 "~/.emacs.d/emacs-plugins/auto-complete-dict")
+			 "~/.emacs.d/emacs-plugins/common-settings/auto-complete-dict")
 (ac-config-default)
 (auto-complete-mode)
